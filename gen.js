2ca997eba81b58fa0667017ded55583414c8359a
Kenaley <arthurdonell20@gmail.com>
2023-06-25 05:58:47 +0100

    kn

--- gen.js
+++ gen.js
@@ -19,3 +19,30 @@ const toogleMenu=()=>{
         $('#men-open-btn').removeClass('fa-bars').addClass('fa-close');
     }
 }
+
+const toogleSelection=(type)=>{
+    switch (type,act) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+    
+    
+        default:
+            break;
+    }
+}
