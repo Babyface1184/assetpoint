b237e89e730ebfa3742f6b302b43371ddedbe161
Kenaley <arthurdonell20@gmail.com>
2023-06-25 05:46:57 +0100

    bgvhvh

--- gen.js
+++ gen.js
@@ -2,17 +2,20 @@ let isMenuOpen=false;
 const toogleMenu=()=>{
     if(isMenuOpen){
         isMenuOpen=!isMenuOpen;
-        $('#menu').removeClass('').addClass('');
-        $('#menu').removeClass('').addClass('');
-        $('#menu').css({'display':'none','visibility':'hidden'})
+        $('#menu').removeClass('men-opn').addClass('men-cls');
+        setTimeout(() => {
+            $('#menu').css({'display':'none','visibility':'hidden'})
+        }, 100);
         $('body').css({'overflow':'scroll'});
+        $('#men-open-btn').removeClass('fa-close').addClass('fa-bars');
+        
         
     }
     else if(!isMenuOpen){
         isMenuOpen=!isMenuOpen;
+        $('#menu').removeClass('men-cls').addClass('men-opn');
         $('#menu').css({'display':'block','visibility':'visible'});
         $('body').css({'overflow':'hidden'});
-
-        menu.style.visibility="visible";
+        $('#men-open-btn').removeClass('fa-bars').addClass('fa-close');
     }
 }
